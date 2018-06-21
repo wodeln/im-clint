b0af549238ea61742f8f37db07f146a1e6b18b61
Bolean <wodeln@163.com>
2018-06-21 16:42:27 +0800

    .env使用

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,13 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const dotenv = require('dotenv-webpack');
+
+/*require('dotenv').config({
+    "path": path.join(process.cwd(),".env")
+});
+console.log(process.cwd());
+console.log(path);*/
 
 function resolve(dir) {
     return path.join(__dirname, '..', dir)
@@ -81,5 +88,8 @@ module.exports = {
         net: 'empty',
         tls: 'empty',
         child_process: 'empty'
-    }
+    },
+    plugins: [
+        new dotenv()
+    ]
 }
